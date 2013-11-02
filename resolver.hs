8c6da131f636e7349be6aea2d38c8499ccaab078
Jan Melcher <info@yogularm.de>
2013-11-02 14:50:01 +0100

    Defined conjunctive normal form types

--- resolver.hs
+++ resolver.hs
@@ -1,2 +1,5 @@
 data Formula = Atom String | Or Formula Formula | And Formula Formula | Not Formula deriving (Eq, Show, Read)
 
+data Literal = PositiveLiteral String | NegativeLiteral String deriving (Eq, Show, Read)
+type Clause = [Literal]
+type ClauseSet = [Clause]
