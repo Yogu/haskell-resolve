3f570bb0d58e41612cbf7bdd23abcb306d1984f9
Jan Melcher <info@yogularm.de>
2013-11-02 18:17:03 +0100

    Implemented formula parser

--- Resolver.hs
+++ Resolver.hs
@@ -4,13 +4,16 @@ module Resolver
   formulaToClauseSet,
   (.&), (.|), nt, a,b,c,f,f2
 ) where 
-  
+
+-- Type declarations  
 
 data Formula = Atom String | Or Formula Formula | And Formula Formula | Not Formula deriving (Eq, Show, Read)
 
 data Literal = PositiveLiteral String | NegativeLiteral String deriving (Eq, Show, Read)
 type Clause = [Literal]
 type ClauseSet = [Clause]
+
+-- CNF formula to clause set
 
 cnfToLiteral :: Formula -> Literal
 cnfToLiteral (Atom name) = PositiveLiteral name
@@ -23,6 +26,8 @@ cnfToClause formula = [cnfToLiteral formula]
 cnfToClauseSet :: Formula -> ClauseSet
 cnfToClauseSet (And left right) = (cnfToClauseSet left) ++ (cnfToClauseSet right)
 cnfToClauseSet formula = [cnfToClause formula]
+
+-- Any formula to CNF formula
 
 shiftNegations :: Formula -> Formula
 shiftNegations (Not (And left right)) = Or (shiftNegations (Not left)) (shiftNegations (Not right))
@@ -47,6 +52,63 @@ shiftAndOr f = f
 
 formulaToCNF :: Formula -> Formula
 formulaToCNF = shiftAndOr . shiftNegations
+
+-- Lexer
+
+data Symbol = OpenParenthesis | ClosingParenthesis | AndOperator | OrOperator | NotOperator | Identifier String | EndOfFile deriving (Eq, Show, Read)
+
+scan :: String -> (Symbol, String)
+scan ('(':tail) = (OpenParenthesis, tail)
+scan (')':tail) = (ClosingParenthesis, tail)
+scan ('&':tail) = (AndOperator, tail)
+scan ('|':tail) = (OrOperator, tail)
+scan ('-':tail) = (NotOperator, tail)
+scan "" = (EndOfFile, "")
+scan all
+       | not $ null ident = (Identifier ident, tail)
+       | otherwise        = error $ "invalid char at " ++ tail
+  where ident = takeWhile isLetter all
+        tail  = dropWhile isLetter all
+        isLetter = (`elem` ['a'..'z'])
+
+-- Parser
+
+parseAtomic :: String -> (Formula, String)
+parseAtomic all =
+  case symbol of (Identifier name) -> (Atom name, tail)
+                 OpenParenthesis   -> let (formula, tail2) = parseOr tail;
+                                          (nextSymbol, realTail) = scan tail2 in case nextSymbol of ClosingParenthesis -> (formula, realTail)
+                                                                                                    _ -> error ("')' expected, but " ++ (show nextSymbol) ++ " found")
+                 s -> error("Identifier or '(' expected, but " ++ (show s) ++ " found")
+  where (symbol, tail) = scan all
+
+parseNot :: String -> (Formula, String)
+parseNot all = 
+  case symbol of NotOperator -> let (formula, realTail) = parseNot tail in (Not formula, realTail)
+                 _ -> parseAtomic all
+  where (symbol, tail) = scan all
+
+parseAnd :: String -> (Formula, String)
+parseAnd all = case middleSymbol of AndOperator -> (And first second, realTail)
+                                    _ -> (first, firstTail)
+  where (first, firstTail) = parseNot(all)
+        (middleSymbol, secondTail) = scan(firstTail)
+        (second, realTail) = parseAnd(secondTail)
+
+parseOr :: String -> (Formula, String)
+parseOr all = case middleSymbol of OrOperator -> (Or first second, realTail)
+                                   _ -> (first, firstTail)
+  where (first, firstTail) = parseAnd(all)
+        (middleSymbol, secondTail) = scan(firstTail)
+        (second, realTail) = parseOr(secondTail)
+
+parse :: String -> Formula
+parse str = case nextSymbol of EndOfFile -> formula
+                               _  -> error $ "End of input expected, but " ++ (show nextSymbol) ++ " found"
+ where (formula, tail) = parseOr str
+       (nextSymbol, _) = scan tail
+
+-- High-order functions
 
 formulaToClauseSet :: Formula -> ClauseSet
 formulaToClauseSet = cnfToClauseSet . formulaToCNF
